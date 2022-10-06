5852b8e70b26677c11fcfdffbcd145cc0e78e270
Alona-Liubchenko <aylin888@rambler.ru>
2022-10-06 23:57:52 +0300

    h2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+
+const KEY = '29510449-399a931f33aaf543423460729';
+
+export class App extends Component {
+  state = {};
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?key=29510449-399a931f33aaf543423460729&q=yellow+flowers&image_type=photo'
+    )
+      .then(response => response.json())
+      .then(console.log);
+  }
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit />
+        <ImageGallery />
+        <Button></Button>
+        <Loader />
+        <Modal />
+      </div>
+    );
+  }
+}
